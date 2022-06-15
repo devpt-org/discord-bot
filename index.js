e7e8aa886eb8d6681cf779bc7c2683e5a4b6f265
Ivo Pereira <ivoecpereira@gmail.com>
2022-06-16 02:00:46 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+const { Client, Intents } = require('discord.js');
+const fs = require("fs");
+
+const { DISCORD_TOKEN } = process.env
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const DESTINATION_CHANNEL_ID = '730385705070755982' // #geral
+
+client.once('ready', async () => {
+	console.log('Ready!');
+});
+
+client.login(DISCORD_TOKEN);
+
+client.on('guildMemberAdd', async function (member) {
+    const message = await welcome(member.id)
+    member.guild.channels.get(DESTINATION_CHANNEL_ID).send(message)
+});
+
+const welcome = async function (memberID) {
+    const introPhrases = fetchFormattedPhrases(`./assets/sentences/intro.txt`);
+    const introPhrase = getRandomStringFromCollection(introPhrases).trim();
+    
+    const welcomingPhrases = fetchFormattedPhrases(`./assets/sentences/welcoming.txt`);
+    const welcomingPhrase = getRandomStringFromCollection(welcomingPhrases).trim();
+    
+    const finalPhrase = replacePlaceholders(`${introPhrase}${welcomingPhrase}`, memberID);
+
+    console.log(`[NEW JOIN] ${finalPhrase}`);
+
+    return finalPhrase;
+}
+
+const getRandomStringFromCollection = function (collection) {
+    return collection[Math.floor(Math.random() * collection.length)].trim()
+}
+
+const fetchFormattedPhrases = function (filename) {
+    let phrases = fs.readFileSync(filename, "utf8");
+    return phrases.split(/\r?\n/).map(item => item.replace(/^\s+|\s+$/g, '')); // replace multiple white-spaces by one white-space
+}
+
+const replacePlaceholders = function (phrases, memberID) {
+    const memberIdTag = `<@${memberID}>`
+    return phrases
+        .replace('{MEMBER_ID}', memberIdTag) // replace dynamic user id placeholder by member id
+}
