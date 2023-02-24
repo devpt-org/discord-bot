063636426911f24394935750e62bfcd3d06e77da
Zorkpt <hugopocas@gmail.com>
2023-02-24 09:48:26 +0000

    lint

--- domain/service/commandUseCaseResolver.ts
+++ domain/service/commandUseCaseResolver.ts
@@ -58,6 +58,7 @@ export default class CommandUseCaseResolver {
           channelId: context.channelId,
           message: ":point_right: https://dontasktoask.com/pt-pt/",
         }),
+
       "!oc": async () =>
         new SendMessageToChannelUseCase(deps).execute({
           channelId: context.channelId,
