30503204efd85e6f9b24ee4d984fa493c601f26c
Ivo Pereira <ivoecpereira@gmail.com>
2023-07-02 00:51:09 +0000

    feat(lemmy-autoposter): add support

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { GuildMember, Message, Client, Intents } from "discord.js";
 import * as dotenv from "dotenv";
+import { CronJob } from "cron";
 import SendWelcomeMessageUseCase from "./application/usecases/sendWelcomeMessageUseCase";
 import FileMessageRepository from "./infrastructure/repository/fileMessageRepository";
 import ChatService from "./domain/service/chatService";
@@ -11,6 +12,8 @@ import CommandUseCaseResolver from "./domain/service/commandUseCaseResolver";
 import ChannelResolver from "./domain/service/channelResolver";
 import KataService from "./domain/service/kataService/kataService";
 import CodewarsKataService from "./infrastructure/service/codewarsKataService";
+import ContentAggregatorService from "./domain/service/contentAggregatorService/contentAggregatorService";
+import LemmyContentAggregatorService from "./infrastructure/service/lemmyContentAggregatorService";
 
 dotenv.config();
 
@@ -25,6 +28,7 @@ const chatService: ChatService = new DiscordChatService(client);
 const loggerService: LoggerService = new ConsoleLoggerService();
 const channelResolver: ChannelResolver = new ChannelResolver();
 const kataService: KataService = new CodewarsKataService();
+const lemmyContentAggregatorService: ContentAggregatorService = new LemmyContentAggregatorService();
 const useCaseResolver = new CommandUseCaseResolver({
   messageRepository,
   chatService,
@@ -33,7 +37,56 @@ const useCaseResolver = new CommandUseCaseResolver({
   kataService,
 });
 
-client.once("ready", () => loggerService.log("Ready!"));
+const checkForNewPosts = async () => {
+  loggerService.log("Checking for new posts on content aggregator...");
+
+  const FEED_CHANNEL_ID = "829694016156205056";
+  const lastPosts = await lemmyContentAggregatorService.fetchLastPosts();
+
+  const now = new Date();
+
+  const filteredPosts = lastPosts.filter((post) => {
+    const diff = Math.abs(now.getTime() - post.getCreatedAt().getTime());
+    const minutes = Math.floor(diff / 1000 / 60);
+    return minutes <= 5;
+  });
+
+  const avoidEmbedInLink = (str: string): string => {
+    // convert links to avoid embed (by wrapping them in <>)
+    const regex = /\[([^\]]+)\]\((https?:\/\/[^)]+)\)/g;
+    return str.replace(regex, "[$1](<$2>)");
+  };
+
+  filteredPosts.forEach((post) => {
+    const title = avoidEmbedInLink(post!.getTitle());
+
+    const message = `Novo post no Lemmy: **${title}** (*${post?.getAuthorName()}*)
+
+👉 Ver em: ${post?.getLink()}
+`;
+
+    chatService.sendMessageToChannel(message, FEED_CHANNEL_ID);
+  });
+
+  loggerService.log(`Published ${filteredPosts.length} new posts!`);
+};
+
+const setupCron = () => {
+  /* eslint-disable no-new */
+  new CronJob(
+    // run every 5 minutes
+    "0 */5 * * * *",
+    checkForNewPosts,
+    null,
+    true,
+    "UTC"
+  );
+};
+
+client.once("ready", () => {
+  loggerService.log("Ready!");
+  setupCron();
+});
 
 client.login(DISCORD_TOKEN);
 
---
+++ infrastructure/service/lemmyContentAggregatorService.ts
@@ -0,0 +1,27 @@
+import Post from "../../domain/service/contentAggregatorService/post";
+import ContentAggregatorService from "../../domain/service/contentAggregatorService/contentAggregatorService";
+
+export default class LemmyContentAggregatorService implements ContentAggregatorService {
+  private feedUrl = "https://lemmy.pt/api/v3/post/list?community_name=devpt&limit=10&page=1&sort=New";
+
+  async fetchLastPosts(): Promise<Post[]> {
+    const response = await fetch(this.feedUrl);
+
+    const data = await response.json();
+
+    const unpinnedPosts = data.posts
+      .filter((item: any) => !item.post.featured_community && !item.post.featured_local)
+      .map(
+        (item: any) =>
+          new Post({
+            authorName: item.creator.display_name || item.creator.name,
+            title: item.post.name,
+            link: item.post.ap_id,
+            description: item.post.body,
+            createdAt: new Date(item.post.published),
+          })
+      );
+
+    return unpinnedPosts;
+  }
+}
