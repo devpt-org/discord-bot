25f98bca92356f6eb645e178b9c5432b783e9240
Master Chief <64928908+MrProSpectrumPT@users.noreply.github.com>
2025-09-04 14:49:42 +0100

    Update chatService.ts

--- domain/service/chatService.ts
+++ domain/service/chatService.ts
@@ -1,3 +1,6 @@
+import { EmbedBuilder } from "discord.js";
+
 export default interface ChatService {
   sendMessageToChannel(message: string, channelId: string): void;
+  sendEmbedToChannel(embed: EmbedBuilder, channelId: string, content?: string): void;
 }
